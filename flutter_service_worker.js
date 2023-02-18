bf2230e2ddea33bf3ff9bf51a8d5dfc9b6a190ed
NExPlain <lizhenpi@gmail.com>
2023-02-17 17:33:59 -0800

    Update to support run button

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "16c49467b30724ff245d43bdd82a385c",
 "favicon.ico": "4987d41e5a2f11314ebd36636c0121c1",
-"index.html": "55bd94f1ead829f18f38aa3c27a186a5",
-"/": "55bd94f1ead829f18f38aa3c27a186a5",
-"main.dart.js": "b6bf32ccd33bc1f51601f0d88672eaab",
+"index.html": "f15b2eff9115be3939b613238d6c6084",
+"/": "f15b2eff9115be3939b613238d6c6084",
+"main.dart.js": "0b0b77547dfcb650ed71c49ce3f59eea",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",

ae6ce5386628e6f6a8b442a94f73d3ad60ba3207
NExPlain <lizhenpi@gmail.com>
2023-03-17 11:36:12 -0700

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "16c49467b30724ff245d43bdd82a385c",
 "favicon.ico": "4987d41e5a2f11314ebd36636c0121c1",
-"index.html": "ad2728bda9b1f012c6b6f0e4bd8c6286",
-"/": "ad2728bda9b1f012c6b6f0e4bd8c6286",
-"main.dart.js": "9c69d4ebc8d3b0532e326f9e79477f64",
+"index.html": "b37fee68fda942256d1858de7f22d898",
+"/": "b37fee68fda942256d1858de7f22d898",
+"main.dart.js": "3b8e9dab0158ff365a27fc04eb937d32",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
